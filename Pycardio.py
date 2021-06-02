12a20d9a9b9c22870b0445108285c31e293bfee7
Joseph Guapacho <jjguapacho@gmail.com>
2021-06-02 17:28:27 -0500

    Se agrego el reto 2. Piedra papel o tijera)

--- Pycardio.py
+++ Pycardio.py
@@ -4,26 +4,37 @@ def area_triangulo():
     area = (base*altura)/2
     print("El area del triangulo es: "+ str(area))
 def pie_pap_tij():
-    jugador_1=input("Jugador 1: Escoja entre piedra, papel o tijera: ")
-    jugador_2=input("Jugador 2: Escoja entre piedra, papel o tijera: ")
-    if  jugador_1== "tijera" and jugador_2 == "papel":
-        print("Ganador jugador 1")
-    elif jugador_1== "piedra" and jugador_2 == "tijera":
-        print("Ganador jugador 1")
-    elif jugador_1== "papel" and jugador_2 == "piedra":
-        print("Ganador jugador 1")
-    elif  jugador_2== "tijera" and jugador_1 == "papel":
-        print("Ganador jugador 2")
-    elif jugador_2== "piedra" and jugador_1 == "tijera":
-        print("Ganador jugador 2")
-    elif jugador_2== "papel" and jugador_1 == "piedra":
-        print("Ganador jugador 2")
-    elif jugador_2 == jugador_1:
-        print("Empate")
+    cont_jugador_1 = 0
+    cont_jugador_2 = 0
+    for i in range(3):
+        jugador_1=input("Jugador 1: Escoja entre piedra, papel o tijera: ")
+        jugador_2=input("Jugador 2: Escoja entre piedra, papel o tijera: ")
+        if  jugador_1== "tijera" and jugador_2 == "papel":
+            print("Ganador jugador 1")
+            cont_jugador_1 += 1
+        elif jugador_1== "piedra" and jugador_2 == "tijera":
+            print("Ganador jugador 1")
+            cont_jugador_1 += 1
+        elif jugador_1== "papel" and jugador_2 == "piedra":
+            print("Ganador jugador 1")
+            cont_jugador_1 += 1
+        elif  jugador_2== "tijera" and jugador_1 == "papel":
+            print("Ganador jugador 2")
+            cont_jugador_2 += 1
+        elif jugador_2== "piedra" and jugador_1 == "tijera":
+            print("Ganador jugador 2")
+            cont_jugador_2 += 1
+        elif jugador_2== "papel" and jugador_1 == "piedra":
+            print("Ganador jugador 2")
+            cont_jugador_2 += 1
+        elif jugador_2 == jugador_1:
+            print("Empate")
+        else:
+            print('La opción no es valida')
+    if cont_jugador_1 > cont_jugador_2:
+        print('El ganador es el jugador 1')
     else:
-        print('La opción no es valida')
-
-
+        print('El ganador es el jugador 2')
 def conv_millas_km():
     pass
 def cal_vol():
